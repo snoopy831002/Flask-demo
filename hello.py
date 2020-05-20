544d4a25727069f40d889ede35a3dbb899a2f326
snoopy831002 <snoopy831002@gmail.com>
2020-05-21 00:30:57 +0800

    Add render demo function

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 from flask import Flask
-from flask import render_template
-
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world(name=None):
-    return render_template('test.html', name=name)
+def hello_world():
+    return 'Hello, World'
