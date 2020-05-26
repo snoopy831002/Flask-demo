a76f117107153243edb5cd97ec635229a856b306
snoopy831002 <snoopy831002@gmail.com>
2020-05-26 22:09:11 +0800

    Added more route for video demo purpose

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def hello_world():
-    return 'Hello, World'
+    return render_template("test.html")
+
+
+@app.route('/hello')
+def hello():
+    return "hello"
+
+@app.route('/hello/<int:userid>')
+def hello_userid(userid):
+    return "hello "+url_for("hello")
