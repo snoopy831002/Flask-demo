7a0ef2513cfcf31be36601634eebe07475d55265
snoopy831002 <snoopy831002@gmail.com>
2020-05-29 01:31:57 +0800

    Fix some bugs

--- hello.py
+++ hello.py
@@ -1,12 +1,31 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_object('config')
+db = SQLAlchemy(app)
 
+class User2(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
 
 @app.route('/', methods=['GET','POST'])
 def hello_world():
-    return render_template("test.html")
+    if(request.method == 'POST'):
+        # save data
+        username = request.form['username']
+        email = request.form['email']
+        user = User2(id=1,username=username,email
+                     =email)
+        db.session.add(user)
+        db.session.commit()
+        return "Updated"
+
+
+    else:
+        return render_template("test.html")
 
 
 @app.route('/hello')
